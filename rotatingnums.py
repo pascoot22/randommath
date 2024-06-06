18e45cf36a72e7f2fccdbd3fc81e016c20362e6a
pascoot22 <pscott1@terpmail.umd.edu>
2024-06-06 09:35:15 -0700

    cleaned rotating nums

--- rotatingnums.py
+++ rotatingnums.py
@@ -1,19 +1,5 @@
 import random
 import time
- 
-# while True:
-#     a = input("Give a five digit num: ")
-#     while int(a) < 10000 or int(a) > 99999:
-#         a = input("no, a five digit number: ")
-#     key = random.randint(10000,99999)
-#     print(key)
-#     num = 0
-#     for i in range(0,5):
-#         sk = str(key)
-
-#         j = (int(sk[i]) + int(a[i])) % 10
-#         num += j * 10 ** (4-i)
-#         print(num)
 
 key = random.randint(1000000000000,9999999999999)
 a = 0
